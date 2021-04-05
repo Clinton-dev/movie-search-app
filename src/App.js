1e8aa614487e8eccbfb48234528cbed10884e21a
clinton wambugu <clintonwambugu@gmail.com>
2021-04-05 15:53:35 +0300

    Add gh pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import MovieSearch from './components/MovieSearch';
 import './App.css';
 
 function App() {
+{/* 
+  1. dont search anything if input field is empty
+  2. give user a msg if there are no movie results
+  3. add a watchlist where user can add their search
+  4. add a btn show description 
+*/}
   return (
     <div className="container">
       <h1 className="title">Movie search app</h1>
--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import MovieCard from './MovieCard';
 
 export default function MoviesSearch() {
+	const [query, setQuery] = useState('');
+	const [movies, setMovies] = useState([]);
+
+	const searchMovie = async (e) => {
+		e.preventDefault();
+		const endpoint = `https://api.themoviedb.org/3/search/multi?api_key=929bd70144cab5f478871bf2f00ec778&language=en-US&query=${query}&page=1&include_adult=false`;
+
+		try {
+	        const res = await fetch(endpoint);
+	        const data  = await res.json();
+	        setMovies(data.results);
+	    }catch(err) {
+			console.log(err);
+	    }
+	}
+
+	console.log(movies)
+	
 	return(
-		<form>
-			<label htmlFor="query">movie name</label>
-			<input type="text" name="query" id="searchQuery" placeholder="i.e game of thrones"/>
-			<button>search</button>
-		</form>
+		<div>
+			<form onSubmit={searchMovie}>
+				<label htmlFor="query">movie name</label>
+				<input 
+					type="text" name="query" 
+					id="searchQuery" 
+					placeholder="i.e game of thrones"
+					onChange={e => setQuery(e.target.value)}
+				/>
+				<button>search</button>
+			</form>
+			<div className="card-list">
+				{ movies.filter(movie => movie.poster_path).map( movie => <MovieCard key={movie.id} movie={movie}/>)}
+	   		</div>
+	   </div>
 	);
 }
